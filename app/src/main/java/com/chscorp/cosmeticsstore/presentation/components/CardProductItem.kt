da02a78ebef5d500949d4e0b86f215e0f223c40c
carlosStorari <carlos.storari95@gmail.com>
2024-02-25 17:54:40 -0300

    change card UI

--- app/src/main/java/com/chscorp/cosmeticsstore/presentation/components/CardProductItem.kt
+++ app/src/main/java/com/chscorp/cosmeticsstore/presentation/components/CardProductItem.kt
@@ -2,17 +2,25 @@ package com.chscorp.cosmeticsstore.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.Dp
@@ -20,7 +28,6 @@ import androidx.compose.ui.unit.dp
 import com.chscorp.cosmeticsstore.domain.product.ProductListItem
 import com.chscorp.cosmeticsstore.presentation.ui.theme.CosmeticsStoreTheme
 import com.chscorp.cosmeticsstore.presentation.ui.theme.DeepPeach
-import com.chscorp.cosmeticsstore.presentation.ui.theme.QuickSilver
 import com.chscorp.cosmeticsstore.presentation.ui.theme.WengeGray
 
 @Composable
@@ -43,19 +50,37 @@ fun CardProductItem(
                 .background(DeepPeach)
                 .padding(16.dp)
         ) {
-            Text(
-                text = product.brand ?: "marca indisponivel",
-                color = WengeGray
-            )
-            Text(
-                text = product.price ?: "preço indisponivel",
-                color = WengeGray
-            )
-            Text(
-                text = product.description ?: "descrição indisponivel",
-                color = WengeGray
-            )
-
+            Row {
+                Text(text = "Marca: ", fontWeight = FontWeight.Bold)
+                Text(
+                    text = product.brand ?: "marca indisponivel",
+                    color = WengeGray
+                )
+                Spacer(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxHeight())
+                Icon(Icons.Default.FavoriteBorder, contentDescription = null)
+            }
+            Row {
+                Text(text = "Preço: ", fontWeight = FontWeight.Bold)
+                Text(
+                    text = product.price ?: "preço indisponivel",
+                    color = WengeGray
+                )
+            }
+            Row {
+                Text(
+                    buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                            append("Descrição: ")
+                        }
+                        withStyle(style = SpanStyle(color = WengeGray)) {
+                            append(product.description ?: "descrição indisponivel")
+                        }
+                    }
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/chscorp/cosmeticsstore/presentation/components/HomeListProductScreen.kt
+++ app/src/main/java/com/chscorp/cosmeticsstore/presentation/components/HomeListProductScreen.kt
@@ -1,10 +1,10 @@
 package com.chscorp.cosmeticsstore.presentation.components
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,8 +16,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
@@ -28,9 +26,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.chscorp.cosmeticsstore.presentation.ui.ProductState
 import com.chscorp.cosmeticsstore.presentation.ui.theme.DarkCoral
@@ -50,101 +48,105 @@ fun HomeListProductScreenStateful(
 fun HomeListProductScreenStateless(
     state: ProductState = ProductState()
 ) {
-    Column {
-        var productList = state.productList
-        val isLoading = state.isLoading
-        val options = listOf(
-            "Menor preço",
-            "Maior preço",
-            "A-Z",
-            "Z-A",
-            "Favoritos"
-        )
-        var selectedOption by remember {
-            mutableStateOf("")
-        }
-        val onSelectionChange = { text: String ->
-            selectedOption = text
-        }
-        Text(text = "Ordenar por")
-        Spacer(modifier = Modifier)
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .heightIn(70.dp)
-                .horizontalScroll(rememberScrollState())
-        ) {
-            options.forEach { text ->
-                Row(
-                    modifier = Modifier
-                        .padding(all = 8.dp)
-                ) {
-                    Text(
-                        text = text,
-                        style = typography.bodySmall.merge(),
-                        color = OldBurgundy,
+    val isLoading = state.isLoading
+    if (!isLoading) {
+        Column {
+            var productList = state.productList
+            val options = listOf(
+                "Menor preço",
+                "Maior preço",
+                "A-Z",
+                "Z-A",
+                "Favoritos"
+            )
+            var selectedOption by remember {
+                mutableStateOf("")
+            }
+            val onSelectionChange = { text: String ->
+                selectedOption = text
+            }
+            Text(text = "Ordenar por")
+            Spacer(modifier = Modifier)
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .heightIn(70.dp)
+                    .horizontalScroll(rememberScrollState())
+            ) {
+                options.forEach { text ->
+                    Row(
                         modifier = Modifier
-                            .clip(
-                                shape = RoundedCornerShape(
-                                    size = 12.dp,
-                                ),
-                            )
-                            .clickable {
-                                onSelectionChange(text)
-                            }
-                            .background(
-                                if (text == selectedOption) {
-                                    DarkCoral
-                                } else {
-                                    DeepPeach
+                            .padding(all = 8.dp)
+                    ) {
+                        Text(
+                            text = text,
+                            style = typography.bodySmall.merge(),
+                            color = OldBurgundy,
+                            modifier = Modifier
+                                .clip(
+                                    shape = RoundedCornerShape(
+                                        size = 12.dp,
+                                    ),
+                                )
+                                .clickable {
+                                    onSelectionChange(text)
                                 }
-                            )
-                            .padding(
-                                vertical = 12.dp,
-                                horizontal = 16.dp,
-                            ),
-                    )
+                                .background(
+                                    if (text == selectedOption) {
+                                        DarkCoral
+                                    } else {
+                                        DeepPeach
+                                    }
+                                )
+                                .padding(
+                                    vertical = 12.dp,
+                                    horizontal = 16.dp,
+                                ),
+                        )
+                    }
                 }
             }
-        }
-        when (selectedOption) {
-            "Menor preço" -> {
-                productList = productList?.sortedBy { it.price?.toFloat() }
-            }
+            when (selectedOption) {
+                "Menor preço" -> {
+                    productList = productList?.sortedBy { it.price?.toFloat() }
+                }
 
-            "Maior preço" -> {
-                productList = productList?.sortedByDescending { it.price?.toFloat() }
-            }
+                "Maior preço" -> {
+                    productList = productList?.sortedByDescending { it.price?.toFloat() }
+                }
 
-            "A-Z" -> {
-                productList = productList?.sortedBy { it.brand }
-            }
+                "A-Z" -> {
+                    productList = productList?.sortedBy { it.brand }
+                }
 
-            "Z-A" -> {
-                productList = productList?.sortedByDescending { it.brand }
-            }
+                "Z-A" -> {
+                    productList = productList?.sortedByDescending { it.brand }
+                }
 
-            else -> {}
-        }
-        LazyColumn(
-            Modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            if (productList != null) {
-                for (product in productList) {
-                    item {
-                        CardProductItem(product)
+                else -> {}
+            }
+            LazyColumn(
+                Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                if (productList != null) {
+                    for (product in productList) {
+                        item {
+                            CardProductItem(product)
+                        }
                     }
                 }
             }
         }
-        if (isLoading) {
+    } else {
+        Box(modifier = Modifier.fillMaxSize()) {
             CircularProgressIndicator(
-                modifier = Modifier.width(64.dp),
+                modifier = Modifier
+                    .width(64.dp)
+                    .align(Alignment.Center),
                 color = MaterialTheme.colorScheme.secondary
             )
         }
     }
-
 }
